2821ec9ef7c7de896e049d8e01d77e111897751a
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2024-11-05 20:38:38 -0800

    Settings Position fixed

--- scripts/General/settings.js
+++ scripts/General/settings.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
 
+    $("#Setting-Window").css({
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
+    })
+
+
+    $("#Setting-Btn").on("click", function() {
+        if ($("#Setting-Window").hasClass("hidden")) {
+            $("#Setting-Window").removeClass("hidden");
+            $("#Setting-Window").addClass("flex");
+        } else {
+            $("#Setting-Window").addClass("hidden");
+            $("#Setting-Window").removeClass("flex");
+        }
+    })
+
+    $("#CloseSettings").on("click", function() {
+        $("#Setting-Window").addClass("hidden");
+        $("#Setting-Window").removeClass("flex");
+    })
+
+
+
 })
