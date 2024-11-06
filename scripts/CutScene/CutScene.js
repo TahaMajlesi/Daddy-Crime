a59d01923f2986b6653aa6066a8efd7aeaf9fb58
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2024-11-05 17:17:57 -0800

    Update

--- scripts/CutScene/CutScene.js
+++ scripts/CutScene/CutScene.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
-    paragraph = ``
     let classes = []
+    html = ''
 
     function SceneTyping(id) {
         fetch(`/json/dialogues.json`).then((res) => res.json()).then((data) => {
-            classes = [data[id]["class"]]
-            paragraph = `<p>${data[id]["dialogue"]}</p>`
+            function AddClasses() {
+                $("#Script").addClass(data[id]["class"])
+            }
+
+            function ClearClasses() {
+                $("#Script").removeClass()
+            }
 
 
-
-
-
-            var typed = new Typed('#CutScene', {
-                strings: [paragraph],
-                typeSpeed: data[id]["TypingSpeed"],
-
+            var typed = new Typed('#Script', {
+                strings: [data[id]["dialogue"], "Second sentence."],
+                typeSpeed: 30,
+                showCursor: false,
+                onBegin: AddClasses,
+                onComplete: ClearClasses,
             });
 
 
